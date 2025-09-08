ada3081e806a9cbc83d0c44be94daef6084e0e58
prashu-tech <prashantparsane894@gmail.com>
2025-09-08 12:42:13 +0530

    Update page.jsx

--- src/app/auth-success/page.jsx
+++ src/app/auth-success/page.jsx
@@ -1,26 +1,10 @@
-"use client";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { Suspense } from "react";
+import GoogleCallbackPage from "./GoogleCallbackClient";
 
-export default function GoogleCallbackPage() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    const accessToken = searchParams.get("accessToken");
-    const refreshToken = searchParams.get("refreshToken");
-    const user = searchParams.get("user");
-
-    if (accessToken) {
-      localStorage.setItem("accessToken", accessToken);
-      localStorage.setItem("refreshToken", refreshToken);
-      if (user) localStorage.setItem("user", user);
-
-      router.push("/dashboard");
-    } else {
-      router.push("/login?error=google_failed");
-    }
-  }, [router, searchParams]);
-
-  return <p>Signing you in with Google...</p>;
+export default function Page() {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <GoogleCallbackPage />
+    </Suspense>
+  );
 }
